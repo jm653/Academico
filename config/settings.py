5bbbf396631e1a64cfcf00bde89b4ebeafab9af2
João Marcelo Bachião Vaz Moreira <02314964608@MUZAMBINHO.EDU.BR>
2026-03-30 16:14:21 -0300

    professorrr

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,78 @@
 from pathlib import Path
 import os
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SEGURANÇA
+SECRET_KEY = 'django-insecure-chave-teste'
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+# APPS INSTALADOS
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'app',
+]
+
+# MIDDLEWARE
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# URL PRINCIPAL
+ROOT_URLCONF = 'config.urls'
+
+# TEMPLATES
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'app/templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# BANCO DE DADOS (SQLite simples)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# VALIDAÇÃO DE SENHA
+AUTH_PASSWORD_VALIDATORS = []
+
+# IDIOMA E TEMPO
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
+
+USE_I18N = True
+USE_TZ = True
+
+# ARQUIVOS ESTÁTICOS
+STATIC_URL = '/static/'
+
+# PADRÃO DE ID
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
